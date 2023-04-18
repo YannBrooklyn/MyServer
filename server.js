e7959bb3d4120a6a752d94f544cc3e247c8ad483
YannBrooklyn <yann.massoro@gmail.com>
2023-04-18 16:52:10 +0200

    save

--- server.js
+++ server.js
@@ -1,6 +1,42 @@
-let app = require('express')()
+
+let express = require('express')
+var bodyParser = require('body-parser')
+let app = express()
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+app.set('view engine' , 'ejs')
+
+
+
+app.use('/assets', express.static('public'))
+
 app.get('/', (req, res) => {
-    res.send('Salut')
+    res.render('pages/index')
 })
 
-app.listen(9000)
+app.get('/registrer', (req, res) => {
+    res.render('pages/registrer', {pages: "test"})
+})
+
+app.get('/contact', (req, res) => {
+    res.render('pages/contact')
+})
+
+app.get('/login', (req, res) => {
+    res.render('pages/login')
+})
+
+app.post('/', (req, res) => {
+    console.log(req.body)
+})
+
+app.post('/registrer', (req, res) => {
+    console.log(req.body)
+})
+
+app.listen(9001)
